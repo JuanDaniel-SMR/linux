41f20865614105e2e4375912a1e2d8e7f33e74aa
Juan Daniel <117939198+JuanDaniel-SMR@users.noreply.github.com>
2026-04-17 20:18:10 +0200

    Refactor user prompts to use read -p for clarity

--- scripts/calculadora.sh
+++ scripts/calculadora.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 echo "Bienvenido a mi calculadora."
-echo "Deseas SUMAR, RESTAR, MULTIPLICAR o DIVIDIR?"
-read op
+read -p "Deseas SUMAR, RESTAR, MULTIPLICAR o DIVIDIR?" op
 if [ "$op" = SUMAR ]; then
-	echo "Has elegido sumar, escriba el primer número."
-	read num1
-	echo "Tu primer número es $num, escriba el siguiente numero."
-	read num2
+	read -p "Has elegido sumar, escriba el primer número. " num1
+	read -p "Tu primer número es $num1, escriba el siguiente numero." num2
 	resultado=$((num1 + num2))
 	echo "El resultado de tu suma es $resultado."
 
 elif [ "$op" = RESTAR ]; then
-        echo "Has elegido restar, escriba el primer número."
-        read num1
-        echo "Tu primer número es $num1, escriba el siguiente numero."
-        read num2
+        read -p "Has elegido restar, escriba el primer número." num1
+        read -p "Tu primer número es $num1, escriba el siguiente numero." num2
         resultado=$((num1 - num2))
         echo "El resultado de tu resta es $resultado."
 
 elif [ "$op" = MULTIPLICAR ]; then
-        echo "Has elegido multiplicar, escriba el primer número."
-        read num1
-        echo "Tu primer número es $num1, escriba el siguiente numero."
-        read num2
+        read -p "Has elegido multiplicar, escriba el primer número." num1
+        read -p "Tu primer número es $num1, escriba el siguiente numero." num2
         resultado=$((num1 * num2))
         echo "El resultado de tu multiplicación es $resultado."
 
 elif [ "$op" = DIVIDIR ]; then
-        echo "Has elegido dividir, escriba el primer número."
-        read num1
-        echo "Tu primer número es $num1, escriba el siguiente numero."
-        read num2
+        read -p "Has elegido dividir, escriba el primer número." num1
+        read -p "Tu primer número es $num1, escriba el siguiente numero." num2
         resultado=$((num1 / num2))
         echo "El resultado de tu división es $resultado."
 
