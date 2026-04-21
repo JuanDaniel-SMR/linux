cff591d8159b54767ad017fdf2d9a0f67da43b81
Juan Daniel <117939198+JuanDaniel-SMR@users.noreply.github.com>
2026-04-21 14:09:38 +0200

    Update triangle_area.sh

--- scripts/triangle_area.sh
+++ scripts/triangle_area.sh
@@ -2,7 +2,7 @@
 echo "Bienvenido a calcular el área de un triangulo."
 echo "Indique la altura de su triangulo."
 read height
-echo "Indique la base de su traingulo."
+echo "Indique la base de su triangulo."
 read base
 triangle_area=$((height * base /2))
 echo "El área de su triangulo es $triangle_area"
