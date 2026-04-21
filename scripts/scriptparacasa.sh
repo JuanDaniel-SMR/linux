5f538f6c12d90b9a6683442549d3478ee133802c
Juan Daniel <117939198+JuanDaniel-SMR@users.noreply.github.com>
2026-04-21 20:04:57 +0200

    Update scriptparacasa.sh

--- scripts/scriptparacasa.sh
+++ scripts/scriptparacasa.sh
@@ -2,16 +2,18 @@
 read -p "Indique cual es su nombre"$'\n' name
 fecha=$(date +%A)
 echo "Buenas $name, hoy es $fecha."
-	if [ ! -f "nombre.txt" ']; then
+	if [ ! -f "nombre.txt" ]; then
 	touch nombre.txt	
 	else	
 	echo "El archivo ya existe."
 	fi
 echo $name >> "nombre.txt"
 mv "nombre.txt" "NAME.TXT"
-	if [ ! -d "PERSONAS" ']; then
+	if [ ! -d "PERSONAS" ]; then
 	mkdir "PERSONAS"
 	else
 	echo "El directorio PERSONAS ya existe, actualizando actual."
 	fi
-mv "NAME.TXT" PERSONAS
+mv "NAME.TXT" "PERSONAS"
+echo "Su nombre ha sido registrado en el fichero NAME.TXT dentro de la carpeta PERSONAS."
+chmod a+rw,a-x "PERSONAS/NAME.TXT"
